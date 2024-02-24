ce5fdd08cf2c4d71074b15072cd7a22149f185d9
Allmalik05 <pasermbilegi123@gmail.com>
2024-02-25 00:20:27 +0700

    revisi js-2

--- cek-tiket.js
+++ cek-tiket.js
@@ -21,8 +21,8 @@ function functionKlik() {
         const list_studio = ["A", "B", "C"];
         let pilih_studio = prompt("Silahkan Pilih Studio Anda (A/B/C):");
 
-        while(pilih_studio === "") {
-            alert("Mohon pilih salah satu studio diatas.")
+        while(pilih_studio === "" || (!list_studio.includes(pilih_studio.toUpperCase()))) {
+            alert("Mohon maaf pilihan anda tidak ada dalam daftar \n Pilih salah satu studio diatas.")
             pilih_studio = prompt("Silahkan Pilih Studio Anda (A/B/C):");
         }
         if(list_studio.includes(pilih_studio.toUpperCase())) {
@@ -45,9 +45,9 @@ function functionKlik() {
         // }else if (pilih_studio == list_studio[2]) {
         //     alert(`Tiket atas nama ${input_nama} dengan umur ${input_umur}, silahkan memasuki studio C`);
         // }
-        else {
-            alert("Maaf pilihan anda tidak ada dalam daftar. \n silahkan input ulang!");
-        }
+        // else {
+        //     alert("Maaf pilihan anda tidak ada dalam daftar. \n silahkan input ulang!");
+        // }
     }else {
         alert("Mohon maaf usia anda tidak memenuhi syarat masuk, harus di atas 13 tahun.");
     }
